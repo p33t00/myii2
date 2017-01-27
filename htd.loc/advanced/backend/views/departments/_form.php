a289f100d168bd1c4237657f2aadb84945b4a0ec
Peet <p33tsjash41k@hotmail.com>
2017-01-28 00:27:45 +0300

    Added "Select2" widged to Departments create form, branch_id field.

--- htd.loc/advanced/backend/views/departments/_form.php
+++ htd.loc/advanced/backend/views/departments/_form.php
@@ -4,6 +4,8 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use \yii\helpers\ArrayHelper;
 use backend\models\Companies;
+use backend\models\Branches;
+use kartik\select2\Select2;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Departments */
@@ -14,9 +16,17 @@ use backend\models\Companies;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'branch_id')->textInput() ?>
-
     <?= $form->field($model, 'dept_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'branch_id')->widget(Select2::classname(), [
+    'data' => ArrayHelper::map(Branches::find()->all(), 'branch_id', 'branch_name'),
+    'language' => 'de',
+    'options' => ['placeholder' => 'Select a state ...'],
+    'pluginOptions' => [
+    'allowClear' => true
+    ],
+    ]);
+    ?>
 
     <?= $form->field($model, 'company_id')->dropDownList(
         ArrayHelper::map(Companies::find()->all(), 'company_id', 'company_name')
